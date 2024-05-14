3dd66fe8df99437243a23f8efa9acabf5b337ea1
oregano29 <159599688+oregano29@users.noreply.github.com>
2024-05-14 01:04:59 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,17 @@
+                                                                                                                                                                                                             /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.app;
+
+/**
+ *
+ * @author isaac
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Formularios.FormPrincipal objetoFormulario = new Formularios.FormPrincipal();
+        objetoFormulario.setVisible(true);
+    }
+}
